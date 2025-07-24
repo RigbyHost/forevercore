826d8ca12d55593f5dcf2294e676a6162af2d9da
FimastGD <daniil.port135@gmail.com>
2025-07-25 00:00:38 +0300

    getAccountURL multi connection

--- api/lib/getAccountURL.ts
+++ api/lib/getAccountURL.ts
@@ -1,4 +1,4 @@
-import { Request } from 'express';
+import { Request } from "express";
 
 /**
  * Gets the protocol (http or https) from the request
@@ -6,10 +6,10 @@ import { Request } from 'express';
  * @returns Protocol string (http or https)
  */
 const getAccountURL = async (req: Request): Promise<string> => {
-  return new Promise((resolve) => {
-    const protocol = (req.secure || req.headers['x-forwarded-proto'] === 'https') ? 'https' : 'http';
-    resolve(protocol);
-  });
+	return new Promise(resolve => {
+		const protocol = req.secure || req.headers["x-forwarded-proto"] === "https" ? "https" : "http";
+		resolve(protocol);
+	});
 };
 
 export default getAccountURL;
