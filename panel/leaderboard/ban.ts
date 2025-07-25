a021974a70671048ed0e8913e1d1b1263bb17c05
FimastGD <daniil.port135@gmail.com>
2025-07-25 14:02:10 +0300

    [panel] leaderboard rewrite

--- panel/leaderboard/ban.ts
+++ panel/leaderboard/ban.ts
@@ -1,9 +1,9 @@
-'package net.fimastgd.forevercore.panel.leaderboard.ban';
+"package net.fimastgd.forevercore.panel.leaderboard.ban";
 
-import { Connection, RowDataPacket, ResultSetHeader, FieldPacket } from 'mysql2/promise';
-const db: Connection = require("../../serverconf/db");
-const ConsoleApi = require("../../modules/console-api");
-const c = require("ansi-colors");
+import { RowDataPacket, ResultSetHeader, FieldPacket } from "mysql2/promise";
+import threadConnection from "../../serverconf/db";
+import ConsoleApi from "net.fimastgd.forevercore.modules.console-api";
+import * as c from "ansi-colors";
 
 type int = number;
 
@@ -11,49 +11,47 @@ type int = number;
 // -1 = account not found
 // -2 = unknown error
 
-async function banUser(userName: string): Promise<int> {
-    async function getAccountID(userName: string): Promise<int | null> {
-        try {
-            const [rows]: [RowDataPacket[], FieldPacket[]] = await db.execute(
-                "SELECT accountID FROM accounts WHERE userName = ?",
-                [userName]
-            );
-            if (rows.length > 0) {
-                return rows[0].accountID;
-            } else {
-                return null;
-            }
-        } catch (error) {
+async function banUser(gdpsid: string, userName: string): Promise<int> {
+	const db = await threadConnection(gdpsid);
+	async function getAccountID(userName: string): Promise<int | null> {
+		try {
+			const [rows]: [RowDataPacket[], FieldPacket[]] = await db.execute("SELECT accountID FROM accounts WHERE userName = ?", [userName]);
+			if (rows.length > 0) {
+				return rows[0].accountID;
+			} else {
+				return null;
+			}
+		} catch (error) {
 			ConsoleApi.Error("main", `${error} at net.fimastgd.forevercore.panel.leaderboard.ban`);
-            return null;
-        }
-    }
-    
-    try {
-        const accountID = await getAccountID(userName);
-        if (accountID === null) {
-            return -1;
-        }
-        const [updateResult]: [ResultSetHeader, FieldPacket[]] = await db.execute(
-            "UPDATE users SET isBanned = 1 WHERE LOWER(userName) = LOWER(?)",
-         [userName]
-    );
-        /* if (updateResult.affectedRows !== 0) {
+			return null;
+		}
+	}
+
+	try {
+		const accountID = await getAccountID(userName);
+		if (accountID === null) {
+			return -1;
+		}
+		const [updateResult]: [ResultSetHeader, FieldPacket[]] = await db.execute("UPDATE users SET isBanned = 1 WHERE LOWER(userName) = LOWER(?)", [
+			userName
+		]);
+		/* if (updateResult.affectedRows !== 0) {
             //
         } else {
             return -2;
         } */
-        const timestamp = Math.floor(Date.now() / 1000);
-        await db.execute(
-            "INSERT INTO modactions (type, value, value2, timestamp, account) VALUES ('15', ?, '1', ?, ?)",
-            [accountID, timestamp, accountID]
-        );
-        ConsoleApi.Log("main", `Panel action: banned user: ${userName}`);
-        return 1;
-    } catch (error) {
-        ConsoleApi.Error("main", `${error} at net.fimastgd.forevercore.panel.leaderboard.ban`);
-        return -2;
-    }
+		const timestamp = Math.floor(Date.now() / 1000);
+		await db.execute("INSERT INTO modactions (type, value, value2, timestamp, account) VALUES ('15', ?, '1', ?, ?)", [
+			accountID,
+			timestamp,
+			accountID
+		]);
+		ConsoleApi.Log("main", `Panel action: banned user: ${userName}`);
+		return 1;
+	} catch (error) {
+		ConsoleApi.Error("main", `${error} at net.fimastgd.forevercore.panel.leaderboard.ban`);
+		return -2;
+	}
 }
 
-export default banUser; 
+export default banUser;
--- panel/leaderboard/unban.ts
+++ panel/leaderboard/unban.ts
@@ -1,9 +1,9 @@
-'package net.fimastgd.forevercore.panel.leaderboard.unban';
+"package net.fimastgd.forevercore.panel.leaderboard.unban";
 
-import { Connection, RowDataPacket, ResultSetHeader, FieldPacket } from 'mysql2/promise';
-const db: Connection = require("../../serverconf/db");
-const ConsoleApi = require("../../modules/console-api");
-const c = require("ansi-colors");
+import { RowDataPacket, ResultSetHeader, FieldPacket } from "mysql2/promise";
+import threadConnection from "../../serverconf/db";
+import ConsoleApi from "net.fimastgd.forevercore.modules.console-api";
+import * as c from "ansi-colors";
 
 type int = number;
 
@@ -11,44 +11,42 @@ type int = number;
 // -1 = account not found
 // -2 = unknown error
 
-async function unbanUser(userName: string): Promise<int> {
-    async function getAccountID(userName: string): Promise<int | null> {
-        try {
-            const [rows]: [RowDataPacket[], FieldPacket[]] = await db.execute(
-                "SELECT accountID FROM accounts WHERE userName = ?",
-                [userName]
-            );
-            if (rows.length > 0) {
-                return rows[0].accountID;
-            } else {
-                return null;
-            }
-        } catch (error) {
+async function unbanUser(gdpsid: string, userName: string): Promise<int> {
+	const db = await threadConnection(gdpsid);
+	async function getAccountID(userName: string): Promise<int | null> {
+		try {
+			const [rows]: [RowDataPacket[], FieldPacket[]] = await db.execute("SELECT accountID FROM accounts WHERE userName = ?", [userName]);
+			if (rows.length > 0) {
+				return rows[0].accountID;
+			} else {
+				return null;
+			}
+		} catch (error) {
 			ConsoleApi.Error("main", `${error} at net.fimastgd.forevercore.panel.leaderboard.unban`);
-            return null;
-        }
-    }
-    
-    try {
-        const accountID = await getAccountID(userName);
-        if (accountID === null) {
-            return -1;
-        }
-        const [updateResult]: [ResultSetHeader, FieldPacket[]] = await db.execute(
-            "UPDATE users SET isBanned = 0 WHERE LOWER(userName) = LOWER(?)",
-            [userName]
-        );
-        const timestamp = Math.floor(Date.now() / 1000);
-        await db.execute(
-            "INSERT INTO modactions (type, value, value2, timestamp, account) VALUES ('15', ?, '0', ?, ?)",
-            [accountID, timestamp, accountID]
-        );
-        ConsoleApi.Log("main", `Panel action: unbanned user: ${userName}`);
-        return 1;
-    } catch (error) {
+			return null;
+		}
+	}
+
+	try {
+		const accountID = await getAccountID(userName);
+		if (accountID === null) {
+			return -1;
+		}
+		const [updateResult]: [ResultSetHeader, FieldPacket[]] = await db.execute("UPDATE users SET isBanned = 0 WHERE LOWER(userName) = LOWER(?)", [
+			userName
+		]);
+		const timestamp = Math.floor(Date.now() / 1000);
+		await db.execute("INSERT INTO modactions (type, value, value2, timestamp, account) VALUES ('15', ?, '0', ?, ?)", [
+			accountID,
+			timestamp,
+			accountID
+		]);
+		ConsoleApi.Log("main", `Panel action: unbanned user: ${userName}`);
+		return 1;
+	} catch (error) {
 		ConsoleApi.Error("main", `${error} at net.fimastgd.forevercore.panel.leaderboard.unban`);
-        return -2;
-    } 
+		return -2;
+	}
 }
 
-export default unbanUser; 
+export default unbanUser;
