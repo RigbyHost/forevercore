75df3e62e6128e44e455a28dd9ec263a51bf440a
FimastGD <daniil.port135@gmail.com>
2025-07-25 12:24:44 +0300

    bundle system route for custom gdpsid

--- routes/handlers/index.ts
+++ routes/handlers/index.ts
@@ -1,3 +1,5 @@
+`package net.fimastgd.forevercore.handlers`;
+
 import { createAccountHandlers } from './account-handlers';
 import { createCommunicationHandlers } from './message-handlers';
 import { createLevelHandlers } from './level-handlers';
@@ -9,20 +11,22 @@ import { createRewardsHandlers } from './rewards-handlers';
 import { createScoresHandlers } from './score-handlers';
 import { createSystemHandlers } from './system-handlers';
 import { createCommentHandlers } from './comment-handlers';
+import { createFriendshipHandlers } from './friendship-handlers';
 import { ApiHandler } from '../api-router';
 
 export function createAllHandlers(): ApiHandler[] {
-    return [
-        ...createAccountHandlers(),
-        ...createCommunicationHandlers(),
-        ...createLevelHandlers(),
-        ...createListHandlers(),
-        ...createModsHandlers(),
-        ...createOtherHandlers(),
-        ...createPacksHandlers(),
-        ...createRewardsHandlers(),
-        ...createScoresHandlers(),
-        ...createSystemHandlers(),
-        ...createCommentHandlers()
-    ];
+	return [
+		...createAccountHandlers(),
+		...createCommunicationHandlers(),
+		...createLevelHandlers(),
+		...createListHandlers(),
+		...createModsHandlers(),
+		...createOtherHandlers(),
+		...createPacksHandlers(),
+		...createRewardsHandlers(),
+		...createScoresHandlers(),
+		...createSystemHandlers(),
+		...createCommentHandlers(),
+		...createFriendshipHandlers()
+	];
 }
